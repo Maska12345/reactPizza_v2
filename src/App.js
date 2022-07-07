22e570b6bd0a88b52845255fe1445188aac71ce3
Maska12345 <maxgerun@gmail.com>
2022-07-08 00:40:30 +0300

    added pizza`s filters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import pizzas from '../src/assets/pizzas';
+import React from "react";
 
+console.log(pizzas)
 
 function App() {
   return (
@@ -18,8 +21,13 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock title='Мексиканская' price='500'/>
-              <PizzaBlock title='Мясная' price='300'/>
+
+              {
+                pizzas.map((obj)=>(
+                    <PizzaBlock {...obj}/>
+                ))
+              }
+
             </div>
           </div>
         </div>
