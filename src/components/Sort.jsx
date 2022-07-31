6ca6a1481b91e26fbb0ef6670968d7635999bbf3
Maska12345 <maxgerun@gmail.com>
2022-07-31 23:55:48 +0300

    redux-toolkit

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,11 @@
 import React,{useState} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import { setSort } from "../redux/slices/filterSlice";
 
-function Sort({ value,onChangeSort}) {
+function Sort() {
+
+    const dispatch = useDispatch();
+    const sort = useSelector((state)=> state.filter.sort);
 
     const [open,setOpen]=useState(false);
     const list = [
@@ -12,8 +17,8 @@ function Sort({ value,onChangeSort}) {
         {name:'алфавиту asc',sortProperty:'-title'}
         ];
 
-    const onClickListItem = (i) =>{
-        onChangeSort(i);
+    const onClickListItem = (obj) =>{
+        dispatch(setSort(obj));
         setOpen(false);
     };
 
@@ -33,7 +38,7 @@ function Sort({ value,onChangeSort}) {
                     />
                 </svg>
                 <b>Сортировка по:</b>
-                <span onClick={()=>setOpen(!open)}>{value.name}</span>
+                <span onClick={()=>setOpen(!open)}>{sort.name}</span>
             </div>
             {open && (
 
@@ -43,7 +48,7 @@ function Sort({ value,onChangeSort}) {
                         <li
                             key={i}
                             onClick={() => onClickListItem(obj)}
-                            className={value.sortProperty === obj.sortProperty ? 'active' : ''}
+                            className={sort.sortProperty === obj.sortProperty ? 'active' : ''}
                         >{obj.name}</li>
                     ))}
                 </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,30 @@ import PizzaBlock from "../components/PizzaBlock";
 import ReactPaginate from "react-paginate";
 import Pagination from "../components/Pagination";
 import {SearchContext} from "../App";
-
+import { useSelector, useDispatch } from 'react-redux';
+import {setCategoryId} from "../redux/slices/filterSlice";
 
 const Home = () => {
-    const {searchValue} = useContext(SearchContext);
+    const dispatch = useDispatch();
+    const {categoryId,sort} = useSelector((state) => state.filter);
 
+
+
+
+    const {searchValue} = useContext(SearchContext);
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [categoryId, setCategoryId] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
-    const [sortType, setSortType] = useState({name: 'популярности', sortProperty: 'rating'});
+
+    const onChangeCategory = (id) => {
+        dispatch(setCategoryId(id));
+    }
 
     useEffect(() => {
         setIsLoading(true);
 
-        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
-        const sortBy = sortType.sortProperty.replace('-', '');
+        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+        const sortBy = sort.sortProperty.replace('-', '');
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
 
@@ -34,7 +42,7 @@ const Home = () => {
             setIsLoading(false);
         });
         window.scrollTo(0, 0)
-    }, [categoryId, sortType, searchValue, currentPage]);
+    }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
     const pizzas = items.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>));
     const sceletons = [...Array(6)].map((_, index) => <Skeleton key={index}/>);
@@ -42,8 +50,8 @@ const Home = () => {
         <>
             <div className="container">
                 <div className="content__top">
-                    <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)}/>
-                    <Sort value={sortType} onChangeSort={(i) => setSortType(i)}/>
+                    <Categories value={categoryId} onChangeCategory={onChangeCategory}/>
+                    <Sort />
                 </div>
                 <h2 className="content__title">Все пиццы</h2>
                 <div className="content__items">
